8354231b4eff8cdf2e8fa655f3ea2f0ff3e4db5f
rbfbatx <156967200+rbfbatx@users.noreply.github.com>
2025-06-04 15:32:36 -0500

    Update cards.service.ts

--- ptcg-play/src/app/api/services/cards.service.ts
+++ ptcg-play/src/app/api/services/cards.service.ts
@@ -1,17 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { ApiService } from '../api.service';
-import { CardsResponse } from '../interfaces/cards.interface';
+export interface Card {
+  cardNumber: string;
+  gemstone?: string;
+  title: string;
+  type: string;
+  attack1?: string;
+  attack2?: string;
+  attack1Cost?: number;
+  attack2Cost?: number;
+  attack1damage?: string;
+  attack2damage?: string;
+  rarity: string;
+  lp: number;
+  image: string;
+  effect?: string;
+  class?: string;
+}
 
-@Injectable()
+export interface CardsResponse {
+  success: boolean;
+  cards: Card[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
 export class CardsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private api: ApiService,
-  ) {}
-
-  public getAll() {
-    return this.api.get<CardsResponse>('/v1/cards/all');
+  public getCards(): Observable<CardsResponse> {
+    return this.http.get<Card[]>('assets/cards.json').pipe(
+      map(cards => ({ success: true, cards }))
+    );
   }
-
 }
